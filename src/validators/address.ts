104359588e084ad8c3f40e76973004d5dd761c73
serderovsh <steein.shamsudin@gmail.com>
2019-01-04 16:37:15 +0300

    added validator

---
+++ src/validators/address.ts
@@ -0,0 +1,20 @@
+
+import {FormControl} from "@angular/forms";
+
+// Providers
+import {AddressProvider} from "@providers/address/address";
+
+export class AddressValidator
+{
+    static addressProvider: AddressProvider;
+
+    constructor(addressProvider: AddressProvider) {
+        AddressValidator.addressProvider = addressProvider;
+    }
+
+    isValid(control: FormControl) {
+        return AddressValidator.addressProvider.validateAddress(control.value)
+            ? null
+            : { 'Invalid Address': true };
+    }
+}
