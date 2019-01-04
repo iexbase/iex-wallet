09178f53f7c092da6ecd38c154c90c7b4a2c9587
serderovsh <steein.shamsudin@gmail.com>
2019-01-04 17:09:45 +0300

    update AddressValidator

--- src/validators/address.ts
+++ src/validators/address.ts
@@ -1,17 +1,32 @@
 
-import {FormControl} from "@angular/forms";
+import { FormControl } from "@angular/forms";
 
 // Providers
-import {AddressProvider} from "@providers/address/address";
+import { AddressProvider } from "@providers/address/address";
 
 export class AddressValidator
 {
+    /**
+     * Static address provider
+     *
+     * @return AddressProvider
+     */
     static addressProvider: AddressProvider;
 
+    /**
+     * Create a new AddressValidator object
+     *
+     * @param {AddressProvider} addressProvider - Address provider
+     */
     constructor(addressProvider: AddressProvider) {
         AddressValidator.addressProvider = addressProvider;
     }
 
+    /**
+     * Validate Tron address
+     *
+     * @param {FormControl} control - Initializing Form Controls
+     */
     isValid(control: FormControl) {
         return AddressValidator.addressProvider.validateAddress(control.value)
             ? null
